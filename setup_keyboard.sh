dc7c4a3213d9edf350cf087eac3c29681c1fff0f
L0lus <127789984+L0lus@users.noreply.github.com>
2025-12-29 17:27:31 +0100

    Update setup_keyboard.sh
    updating setup script to work on Armbian

--- setup_keyboard.sh
+++ setup_keyboard.sh
@@ -3,17 +3,15 @@ set -e
 
 MODULE_NAME=picocalc_kbd
 SRC_DIR=./picocalc_kbd
-DTBO_DIR=./picocalc_kbd/dts
+DTS_DIR=./picocalc_kbd/dts
 KO_FILE=${MODULE_NAME}.ko
-DTBO_FILE=${MODULE_NAME}.dtbo
+DTS_FILE=${MODULE_NAME}.dts
 
 echo "🔧 Step 1: Installing dependencies..."
 sudo apt update
 sudo apt install -y \
     build-essential \
-    raspberrypi-kernel-headers \
     device-tree-compiler \
-    git
 
 echo "🔧 Step 2: Building kernel module in ${SRC_DIR}..."
 make -C /lib/modules/$(uname -r)/build M=$(realpath ${SRC_DIR}) modules
@@ -23,19 +21,8 @@ sudo mkdir -p /lib/modules/$(uname -r)/extra
 sudo cp ${SRC_DIR}/${KO_FILE} /lib/modules/$(uname -r)/extra/
 sudo depmod
 
-echo "📄 Step 4: Installing DTBO to /boot/overlays/..."
-sudo cp ${DTBO_DIR}/${DTBO_FILE} /boot/overlays/
-
-echo "📝 Step 5: Updating /boot/config.txt..."
-CONFIG=/boot/config.txt
-
-grep -q "^dtoverlay=${MODULE_NAME}" $CONFIG || {
-    sudo sed -i "1i dtoverlay=${MODULE_NAME}" $CONFIG
-}
-
-grep -q "^dtparam=i2c_arm=on" $CONFIG || {
-    sudo sed -i "1i dtparam=i2c_arm=on" $CONFIG
-}
+echo "📄 Step 4: Compiling and adding keyboard dts"
+sudo armbian-add-overlay ${DTS_DIR}/${DTS_FILE} /boot/overlays/
 
 echo "✅ Installation complete."
 echo "🔁 Reboot now to activate the driver:"
